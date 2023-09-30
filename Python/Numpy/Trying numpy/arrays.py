9075158f78f24401a5166c1c289d2c6e8b57e6ac
Omair Hamd alla <o2m0a0i3r@gmail.com>
2023-09-30 19:36:47 +0300

    update

---
+++ Python/Numpy/Trying numpy/arrays.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+arr = np.array([1,2,3,4,5,6])
+print(arr[0:])
+print("This array is: ", arr.ndim ,"D\n")
+
+
+
+arr2D = np.array([[1,2,3],[4,5,6]])
+print(arr[0:][0:])
+print("This array is: ", arr2D.ndim ,"D\n")
+
